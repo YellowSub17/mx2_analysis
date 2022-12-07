c2383015633f2044d97ee3328abffec7d46b6b73
Patrick Adams <patrick_aust@hotmail.com>
2022-12-07 15:26:58 +1100

    Update dl_data.sh

--- dl_data.sh
+++ dl_data.sh
@@ -7,7 +7,7 @@ EMAIL='s3826109@student.rmit.edu.au'
 DATADIR='/data/mx/19754a/frames/riboldia/d'
 SAVEDIR='/beegfs/desy/user/patricka/mx2/data'
 
-FILENAME='lyso_jinxed_alessa_2_13mm*.h5'
+FILENAME='lyso_jinxed_alessa_2_500um*.h5'
 
 echo dl_data.sh: downloading data
 

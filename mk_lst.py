5968bc7599f56d29c44606d4808ce394a3f563b7
Patrick Adams <patrick_aust@hotmail.com>
2022-12-08 17:18:46 +1100

    commit before rm pyutils dir

---
+++ mk_lst.py
@@ -0,0 +1,26 @@
+
+
+
+import os
+
+import glob
+
+
+
+
+DATADIR= '/beegfs/desy/user/patricka/mx2/data'
+CRYSTFELFIR= '/beegfs/desy/user/patricka/mx2/data'
+RUNIDS = ['0091', '0092', '0102', '0103', '0105']
+
+for runid in RUNIDS:
+
+
+
+
+
+
+
+
+
+
+

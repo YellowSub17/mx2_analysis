9c20f0172b2177010f99b56f3bf361bc197401ec
Patrick Adams <patrick_aust@hotmail.com>
2022-12-08 17:47:10 +1100

    x

--- mk_lst.py
+++ mk_lst.py
@@ -12,7 +12,7 @@ DATADIR= '/beegfs/desy/user/patricka/mx2/data'
 CRYSTFELFIR= '/beegfs/desy/user/patricka/mx2/crystfel_calc'
 RUNIDS = ['0091', '0092', '0102', '0103', '0105']
 
-RUNIDS = ['0091']
+RUNIDS = ['0105']
 
 
 

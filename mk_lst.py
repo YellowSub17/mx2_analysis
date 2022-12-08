1f24e649bfafbd4924d44f4be49562fd973b0643
Patrick Adams <patrick_aust@hotmail.com>
2022-12-08 17:37:01 +1100

    x

--- mk_lst.py
+++ mk_lst.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 
 
 import os
@@ -9,11 +9,24 @@ import glob
 
 
 DATADIR= '/beegfs/desy/user/patricka/mx2/data'
-CRYSTFELFIR= '/beegfs/desy/user/patricka/mx2/data'
+CRYSTFELFIR= '/beegfs/desy/user/patricka/mx2/crystfel_calc'
 RUNIDS = ['0091', '0092', '0102', '0103', '0105']
 
-for runid in RUNIDS:
+RUNIDS = ['0091']
+
+
+
+
+for RUNID in RUNIDS:
+    x = glob.glob(f'{DATADIR}/*_{RUNID}_*')
+
+print(x)
     
+
+
+
+
+
 
 
 

0ef4e203f36eb34d587ed953d6174478f6664942
Patrick Adams <patrick_aust@hotmail.com>
2022-12-08 17:25:45 +1100

    cleaned up

--- mk_mask.py
+++ mk_mask.py
@@ -11,20 +11,27 @@ import matplotlib.pyplot as plt
 
 # DATAFILE = '/media/pat/datadrive/mx2tape/h5s/lyso_control1_0087_data_000170.h5'
 # MASKFILE = '/home/pat/Documents/cloudstor/phd/python_projects/mx2_analysis/mask.h5'
+
+#Any random run with hot pixels
 DATAFILE = '/beegfs/desy/user/patricka/mx2/data/lyso_jinxed_alessa_1_0092_data_000001.h5'
+
+#mask file that will be saved
 MASKFILE = '/beegfs/desy/user/patricka/mx2/crystfel_calc/mask.h5'
 
+
+
+#extract data shape
 h5file_data = h5py.File(DATAFILE, 'r')
 NFRAMES, EIGER_NX, EIGER_NY = h5file_data['entry/data/data'].shape
 h5file_data.close()
 
+#10 frames is enough to average over to find hot pixels
 NFRAMES=10
 
 
+#find sum of the frames and sum of frames^2
 run_sum = np.zeros( (EIGER_NX, EIGER_NY) )
 run_sumsq = np.zeros((EIGER_NX, EIGER_NY))
-
-
 
 h5file = h5py.File(DATAFILE, 'r')
 for i in range(NFRAMES):
@@ -33,32 +40,26 @@ for i in range(NFRAMES):
     run_sumsq +=  h5file['/entry/data/data'][i,:,:]**2
 
 
-
 h5file.close()
 
+
+# calculate means and std
 run_mean = run_sum/NFRAMES
 run_std = np.sqrt(run_sumsq/NFRAMES - run_mean**2)
 
 
-
+#location where mean is high and where std is nan is where we want to mask
+# for some reason hot pixels give div 0 error
 loc1 = np.where(run_mean>20)
 loc2 = np.where(np.isnan(run_std))
 
-
+# make mask
 mask = np.zeros((EIGER_NX, EIGER_NY))
-
 mask[loc1]=1
 mask[loc2]=1
 
 
-
-# plt.figure()
-# plt.imshow(run_mean)
-
-
-
-
-
+# save mask
 h5file = h5py.File(MASKFILE, 'w')
 h5file['/entry/data/data'] = mask
 h5file.close()
---
+++ mx2eiger.geom
@@ -0,0 +1,52 @@
+
+; Example geometry file for Eiger 16M detector, using its native file format
+; and binning 2.
+
+; Camera length (in m) and photon energy (eV)
+
+clen = 0.27
+
+photon_energy = 13000
+
+; adu_per_photon needs a relatively recent CrystFEL version.  If your version is
+; older, change it to adu_per_eV and set it to one over the photon energy in eV
+adu_per_photon = 1
+res = 13333.3   ; 75 micron pixel size
+
+; These lines describe the data layout for the Eiger native multi-event files
+dim0 = %
+dim1 = ss
+dim2 = fs
+data = /entry/data/data
+
+
+
+; Uncomment these lines if you have a separate bad pixel map (recommended!)
+mask0_file = /beegfs/desy/user/patricka/mx2/crystfel_calc/mask.h5
+mask0_data = /entry/data/data
+mask0_goodbits = 0
+mask0_badbits = 1
+
+
+
+
+rigid_group_d0 = panel0
+rigid_group_collection_det = d0
+
+; corner_{x,y} set the position of the corner of the detector (in pixels)
+; relative to the beam
+
+panel0/min_fs = 0 
+panel0/min_ss = 0
+panel0/max_fs = 4149
+panel0/max_ss = 4370
+
+panel0/corner_x = -2087.46
+panel0/corner_y = -2230.03
+;panel0/corner_x = -2130.346392
+;panel0/corner_y = -2184.369105
+
+panel0/fs = +1.000000x +0.000000y 
+panel0/ss = +0.000000x +1.000000y
+panel0/coffset = 0
+
